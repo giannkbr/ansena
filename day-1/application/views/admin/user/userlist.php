729528a88b8ea2e9d0d313b955f4c901f455c504
vakbarrr <akbarigi20@gmail.com>
2022-02-14 04:56:50 +0700

    add

--- day-1/application/views/admin/user/userlist.php
+++ day-1/application/views/admin/user/userlist.php
@@ -74,3 +74,142 @@
          </div>
      </div> <!-- end col -->
  </div> <!-- end row -->
+
+
+ <div class="modal fade" id="modal_form" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title mt-0" id="myModalLabel">Add User</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+				</button>
+			</div>
+			<div class="modal-body form">
+				<form method="post" id="form" class="form-horizontal">
+					<input type="hidden" value="" name="id" />
+					<div class="form-body">
+						<div class="form-group">
+							<div class="col-md-9">
+								<input name="id" class="form-control" type="hidden">
+								<span class="help-block" style="color:red"></span>
+							</div>
+						</div>
+
+						<div class="mb-3 row">
+							<div class="mb-3 row">
+								<label for="nama" class="col-md-4 col-form-label">Nama</label>
+								<div class="col-md-8">
+									<input class="form-control" type="text" id="nama" name="nama" placeholder="Nama">
+									<span class="help-block" style="color:red"></span>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="mb-3 row">
+							<div class="mb-3 row">
+								<label for="stok" class="col-md-4 col-form-label">Stok</label>
+								<div class="col-md-8">
+									<input class="form-control" type="text" id="stok" name="stok"
+										placeholder="Stok Barang">
+									<span class="help-block" style="color:red"></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="mb-3 row">
+							<div class="mb-3 row">
+								<label for="harga" class="col-md-4 col-form-label">Harga</label>
+								<div class="col-md-8">
+									<input type="text" placeholder="Harga" id="harga" name="harga" class="form-control">
+									<span class="help-block" style="color:red"></span>
+								</div>
+							</div>
+						</div>
+
+                        <div class="mb-3 row">
+							<div class="mb-3 row">
+								<label for="harga" class="col-md-4 col-form-label">Tanggal Masuk</label>
+								<div class="col-md-8">
+									<input type="text" placeholder="Tanggal masuk" id="tanggal_masuk" name="tanggal_masuk" class="form-control">
+									<span class="help-block" style="color:red"></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="mb-3 row">
+							<div class="mb-3 row">
+								<label for="image" class="col-md-4 col-form-label">Upload Photo</label>
+								<div class="col-md-8">
+									<input class="form-control" type="file" id="foto_barang" name="foto_barang">
+									<span class="help-block" style="color:red"></span>
+								</div>
+							</div>
+						</div>
+
+						<div class="mb-3 row">
+							<div class="mb-3 row" id="photo-preview">
+								<label for="photo" class="col-md-4 col-form-label">Photo</label>
+								<div class="col-md-9">
+									(No photo)
+									<span class="help-block" style="color:red"></span>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button type="button" id="btnSave" onclick="save()" class="btn btn-primary btn-responsive"
+					redirect(site_url().'user/userList');>Simpan </button> <button type="button"
+					class="btn btn-light waves-effect" data-bs-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+    var base_url = '<?php echo base_url(); ?>';
+	var save_method;
+	var table;
+
+    $(document).ready(function () {
+        table = $('#table').DataTable({
+
+        "processing": true,
+        "serverSide": true,
+        "order": [],
+
+        "ajax": {
+            "url": "<?php echo base_url('admin/ajax_list') ?>",
+            "type": "POST"
+        },
+
+        "columnDefs": [{
+                "targets": [0],
+                "orderable": false,
+            },
+            {
+                "targets": [-1],
+                "orderable": false,
+            },
+        ],
+
+        });
+
+        function add_user() {
+            save_method = 'add';
+            $('#form')[0].reset();
+            $('.form-group').removeClass('has-error');
+            $('.help-block').empty();
+            $('#modal_form').modal('show');
+            $('.modal-title').text('Add barang');
+
+            $('#photo-preview').hide(); // hide photo preview modal
+
+            $('#label-photo').text('Upload Photo'); // label photo upload
+        }
+
+    });
+</script>
